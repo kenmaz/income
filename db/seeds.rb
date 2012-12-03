1b1dae93c7fad387f6cb3a0f7a9cf17940afd9c6
Kentaro Matsumae <kentaro.matsumae@gmail.com>
2012-12-04 02:33:00 +0900

    fb,twボタン,広告を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "csv"
+
+CSV.foreach("#{Rails.root}/db/default.csv") do |row|
+  Bonus.create(:company_name => row[0], :amount => row[1], :age => row[2])
+end
