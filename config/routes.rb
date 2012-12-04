be1b71b0f77e9efabea6465c6e243dd66922e06f
Kentaro Matsumae <kentaro.matsumae@gmail.com>
2012-12-05 01:41:03 +0900

    ボーナス登録ボタンサイズを大きく＆トップページをリストにした

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ BonusRanking::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  match '/' => 'bonuses#register'
+  match '/' => 'bonuses#list'
   match '/list' => 'bonuses#list'
+  match '/register' => 'bonuses#register'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
